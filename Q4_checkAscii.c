5791b74b17391652dbfcfa81c87015d929161a89
Scythe <rajpawar220207@gmail.com>
2026-02-14 00:01:52 +0530

    Add files via upload

---
+++ Q4_checkAscii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	//Q4.WAP to print ascii value of any given character.
+	
+	char ch;
+	printf("\nEnter the character to check its Ascii value=");
+	scanf("%c",&ch);
+	
+	printf("\n The Ascii value = %d",ch);
+	
+	return 0;
+}
